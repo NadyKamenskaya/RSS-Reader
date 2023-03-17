73e3bbe84a7653fd9206a44939399dbc7d7855d3
Nadezhda Kamenskaya <nady.kamenskaya@gmail.com>
2023-03-17 20:53:33 +0300

    Add new error

--- src/ru.js
+++ src/ru.js
@@ -5,6 +5,7 @@ export default {
     networkError: 'Ошибка сети',
     rssInvalid: 'Ресурс не содержит валидный RSS',
     isEmpty: 'Не должно быть пустым',
+    unknown: 'Что-то пошло не так',
     success: 'RSS успешно загружен',
     button: 'Просмотр',
     posts: 'Посты',
